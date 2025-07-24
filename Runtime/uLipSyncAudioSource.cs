edc8a18c8b0a92b3e2877c692a126b1e9cfe2556
James Marr <jmarr@moondust.media>
2025-07-23 18:20:40 -0700

    Prevent exception from delivering audio data to a disabled component

--- Runtime/uLipSyncAudioSource.cs
+++ Runtime/uLipSyncAudioSource.cs
@@ -6,13 +6,34 @@ namespace uLipSync
 [RequireComponent(typeof(AudioSource))]
 public class uLipSyncAudioSource : MonoBehaviour
 {
+    bool IsEnabled;
+        
     public AudioFilterReadEvent onAudioFilterRead { get; private set; } = new AudioFilterReadEvent();
 
     void OnAudioFilterRead(float[] input, int channels)
     {
-        if (onAudioFilterRead != null) 
+        lock (this)
         {
-            onAudioFilterRead.Invoke(input, channels);
+            if (onAudioFilterRead != null && IsEnabled)
+            {
+                onAudioFilterRead.Invoke(input, channels);
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        lock (this)
+        {
+            IsEnabled = true;
+        }
+    }
+
+    void OnDisable()
+    {
+        lock (this)
+        {
+            IsEnabled = false;
         }
     }
 }

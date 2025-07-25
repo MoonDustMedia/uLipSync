bf09eecf069244b2ce775614518b4d218bce1c77
James Marr <jmarr@moondust.media>
2025-07-25 08:47:18 -0700

    Revert "Prevent exception from delivering audio data to a disabled component"
    This reverts commit 4fd8aacfa455fcd168319698ad2faa0dd2047a1a.

--- Assets/uLipSync/Runtime/uLipSyncAudioSource.cs
+++ Assets/uLipSync/Runtime/uLipSyncAudioSource.cs
@@ -6,34 +6,13 @@ namespace uLipSync
 [RequireComponent(typeof(AudioSource))]
 public class uLipSyncAudioSource : MonoBehaviour
 {
-    bool IsEnabled;
-        
     public AudioFilterReadEvent onAudioFilterRead { get; private set; } = new AudioFilterReadEvent();
 
     void OnAudioFilterRead(float[] input, int channels)
     {
-        lock (this)
+        if (onAudioFilterRead != null) 
         {
-            if (onAudioFilterRead != null && IsEnabled)
-            {
-                onAudioFilterRead.Invoke(input, channels);
-            }
-        }
-    }
-
-    void OnEnable()
-    {
-        lock (this)
-        {
-            IsEnabled = true;
-        }
-    }
-
-    void OnDisable()
-    {
-        lock (this)
-        {
-            IsEnabled = false;
+            onAudioFilterRead.Invoke(input, channels);
         }
     }
 }
